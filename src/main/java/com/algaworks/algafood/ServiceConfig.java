18a611ea7d34aa0fd5887464ecb0f70f9504a195
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-15 22:40:36 -0300

    Pontos de Injecao de dependencia -> Construtor, Set e Atributo. Os tres podem ter @Autowired. @Bean so e necessario se voce quiser instanciar componentes (com construtor) com configuracoes adicionais.

--- src/main/java/com/algaworks/algafood/ServiceConfig.java
+++ src/main/java/com/algaworks/algafood/ServiceConfig.java
@@ -1,17 +1,11 @@
 package com.algaworks.algafood;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.algaworks.algafood.notifications.Notificador;
-import com.algaworks.algafood.services.AtivacaoClienteService;
-
-@Configuration
-public class ServiceConfig {
-
-	@Bean
-    AtivacaoClienteService ativacaoClienteService(Notificador notificador) {
-    	return new AtivacaoClienteService(notificador);
-    }	
-	
-}
+//@Configuration
+//public class ServiceConfig {
+//
+//	@Bean
+//    AtivacaoClienteService ativacaoClienteService() {
+//    	return new AtivacaoClienteService();
+//    }	
+//	
+//}
--- src/main/java/com/algaworks/algafood/services/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/services/AtivacaoClienteService.java
@@ -6,19 +6,23 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.entities.Cliente;
 import com.algaworks.algafood.notifications.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 
 	@Autowired
 	private Notificador notificador;
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		System.out.println("AtivacaoClienteService: "+notificador);
-	}
-	
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+//	
+//	public AtivacaoClienteService(String qualquer) {
+//			
+//	}
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
-	
+
 }
