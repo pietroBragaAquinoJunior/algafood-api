df3859c886cd0c08551b72084c24f2ce0aecd16a
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-20 16:21:51 -0300

    Propriedades do application.properties dentro de classes

--- src/main/java/com/algaworks/algafood/notifications/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notifications/NotificadorEmail.java
@@ -1,21 +1,23 @@
 package com.algaworks.algafood.notifications;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.entities.Cliente;
 
-@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail Real");
-	}
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
 		System.out.printf("Notificando %s através do e-mail %s usando SMTP: %s\n",
 			cliente.getNome(), cliente.getEmail(), mensagem);
 	}
