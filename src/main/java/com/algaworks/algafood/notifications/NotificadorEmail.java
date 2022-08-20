a570a56f947655fe3cbb4b4b7d7d17ce0ba2ed53
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-20 16:29:39 -0300

    Criando uma classe pra receber as propriedades do application.properties e depois injetando essa classe em outra para ser utilizada sem precisar ficar injetando um atributo de cada vez

--- src/main/java/com/algaworks/algafood/notifications/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notifications/NotificadorEmail.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.notifications;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.entities.Cliente;
@@ -9,18 +9,15 @@ import com.algaworks.algafood.entities.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
+	@Autowired
+	private NotificadorProperties notificadorProperties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Host: " + host);
-		System.out.println("Porta: " + porta);
+		System.out.println("Host: " + notificadorProperties.getHostServidor());
+		System.out.println("Porta: " + notificadorProperties.getPortaServidor());
 		System.out.printf("Notificando %s através do e-mail %s usando SMTP: %s\n",
 			cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
-	
 }
