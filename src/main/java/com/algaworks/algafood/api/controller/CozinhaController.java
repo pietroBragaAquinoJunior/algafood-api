bc885666d63a91a28e513eb15dadb7223b6bd545
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-21 15:12:14 -0300

    Usando ResponseEntity para controlar melhor os status http

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -25,10 +25,10 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
-	@ResponseStatus(HttpStatus.OK)
+	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		return ResponseEntity.status(HttpStatus.OK).body(cozinhaRepository.buscar(cozinhaId));
 	}
 	
 }
