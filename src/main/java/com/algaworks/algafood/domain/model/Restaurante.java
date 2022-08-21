9a2696fdb831641a76ba73f8e904eca7de016850
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-20 22:12:19 -0300

    import.sql

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -20,6 +20,7 @@ public class Restaurante implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	
 	@Column(name="taxa_frente")
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into tb_cozinha (nome) values ('Tailandesa');
+insert into tb_cozinha (nome) values ('Indiana');
